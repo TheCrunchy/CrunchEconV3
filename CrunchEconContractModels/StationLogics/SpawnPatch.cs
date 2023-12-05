bc57c2fdb7882b94be0b303db0a87fefaa4afb27
TheCrunchy <crunchycamsta@gmail.com>
2023-12-05 20:07:09 +0000

    .

--- CrunchEconContractModels/StationLogics/SpawnPatch.cs
+++ CrunchEconContractModels/StationLogics/SpawnPatch.cs
@@ -8,6 +8,7 @@ using CrunchEconV3;
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.Game.Entities;
 using Sandbox.Game.Entities.Cube;
+using Sandbox.Game.GameSystems;
 using Sandbox.Game.SessionComponents;
 using Sandbox.ModAPI;
 using SpaceEngineers.Game.Entities.Blocks;
@@ -46,11 +47,43 @@ namespace CrunchEconContractModels.StationLogics
             ref Vector3 forward,
             ref Vector3 up)
         {
-            Vector3D vector3D1 = new Vector3D(0, 0, 0);
             forward = Vector3.Forward;
             up = Vector3.CalculatePerpendicularVector(forward);
-            position = Sandbox.Game.Entities.MyEntities.FindFreePlace(vector3D1, 5000, 20, 5, 1f) ?? vector3D1;
-            Core.Log.Info("Test");
+            var amountOfVectors = 50;
+            var max = 500;
+            List<Vector3> randomVectors = GenerateRandomVectors(amountOfVectors, max);
+
+            foreach (var item in randomVectors)
+            {
+                var tempposition = Sandbox.Game.Entities.MyEntities.FindFreePlace(item, 2000, 20, 5, 1f);
+                if (tempposition != null)
+                {
+                    position = tempposition.Value;
+                    return;
+                }
+            }
+        }
+
+
+        static List<Vector3> GenerateRandomVectors(int numberOfVectors, float maxDistance)
+        {
+            List<Vector3> vectors = new List<Vector3>();
+
+            for (int i = 0; i < numberOfVectors; i++)
+            {
+                float x = (float)(Core.random.NextDouble() - 0.5) * 2 * maxDistance;
+                float y = (float)(Core.random.NextDouble() - 0.5) * 2 * maxDistance;
+                float z = (float)(Core.random.NextDouble() - 0.5) * 2 * maxDistance;
+
+                Vector3 vector = new Vector3(x, y, z);
+                if (!MyGravityProviderSystem.IsPositionInNaturalGravity(vector))
+                {
+                    vectors.Add(vector);
+                }
+               
+            }
+
+            return vectors;
         }
     }
 }
