d2cde1c2e41f6761a69886fd727f6baf8c71eade
TheCrunchy <crunchycamsta@gmail.com>
2024-01-03 08:38:43 +0000

    start adding the nexus helper

---
+++ CrunchEconContractModels/Random Stuff/NexusHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch.Managers.PatchManager;
+
+namespace CrunchEconContractModels.Random_Stuff
+{
+    [PatchShim]
+    public static class NexusHelper
+    {
+        public static void Patch(PatchContext ctx)
+        {
+        }
+
+        public static Action<byte[]> MessageRecieved;
+
+        public static void SendToChat(string author, string message, string color)
+        {
+
+        }
+
+        private static void SendToChatIngame()
+        {
+
+        }
+
+        public static void SendToNexus(byte[] data)
+        {
+
+        }
+
+        public static void ReceiveFromNexus()
+        {
+
+        }
+    }
+}
