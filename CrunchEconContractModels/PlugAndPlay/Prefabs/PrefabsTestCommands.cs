a91dcc2b1e4a12e5c9b28706a070df73925c5c9b
TheCrunchy <crunchycamsta@gmail.com>
2024-05-26 19:35:03 +0100

    .

--- CrunchEconContractModels/PlugAndPlay/Prefabs/PrefabsTestCommands.cs
+++ CrunchEconContractModels/PlugAndPlay/Prefabs/PrefabsTestCommands.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using CrunchEconContractModels.PlugAndPlay.Helpers;
 using Sandbox.Game.Contracts;
 using Sandbox.Game.Entities;
 using Sandbox.Game.World;
@@ -18,51 +20,25 @@ namespace CrunchEconContractModels.PlugAndPlay.Prefabs
         {
 
         }
-        [Command("testprefab2", "try spawn a prefab")]
+
+        private List<string> Used = new List<string>();
+        [Command("prefab", "try spawn a prefab")]
+        [Permission(MyPromoteLevel.Admin)]
+        public void ExportStore(string prefab)
+        {
+            MyPrefabManager.Static.SpawnPrefab(prefab, Context.Player.Character.PositionComp.GetPosition(), Vector3.Forward, Vector3.Up, ownerId: Context.Player.IdentityId);
+        }
+
+        [Command("combats", "try spawn a prefab")]
         [Permission(MyPromoteLevel.Admin)]
         public void ExportStore()
         {
-            MyContractRepair myContractRepair = new MyContractRepair();
-            var definition = myContractRepair.GetDefinition();
 
-            // Get the type of the internal class
-            Type definitionType = definition.GetType();
+            var randomCombat = PrefabHelper.CombatAttack.GetAllPrefabs();
+            var random = randomCombat.Where(x => !Used.Contains(x)).ToList();
 
-            // Check if the type name matches the internal class name
-            if (definitionType.FullName == "Sandbox.Definitions.MyContractTypeRepairDefinition")
-            {
-                // Use reflection to get the 'PrefabNames' field
-                var prefabNamesField = definitionType.GetField("PrefabNames", BindingFlags.Public | BindingFlags.Instance);
-                if (prefabNamesField != null)
-                {
-                    var prefabNames = prefabNamesField.GetValue(definition) as List<string>;
-                    if (prefabNames != null)
-                    {
-                      //  Context.Respond(string.Join(",", prefabNames));
-                        var resultList = new List<MyCubeGrid>();
-                        Stack<Action> Callbacks = new Stack<Action>();
-                        Callbacks.Push(() =>
-                        {
-                            Context.Respond($"{resultList.Count}");
-                            Context.Respond("Callback");
-                        });
-                        MyPrefabManager.Static.SpawnPrefab(resultList,prefabNames.GetRandomItemFromList(), Context.Player.Character.PositionComp.GetPosition(), Vector3.Forward, Vector3.Up, ownerId:Context.Player.IdentityId, callbacks:Callbacks);
+            MyPrefabManager.Static.SpawnPrefab(random.GetRandomItemFromList(), Context.Player.Character.PositionComp.GetPosition(), Vector3.Forward, Vector3.Up, ownerId: Context.Player.IdentityId);
 
-                    }
-                    else
-                    {
-                        Context.Respond("PrefabNames field is null or not a string array.");
-                    }
-                }
-                else
-                {
-                    Context.Respond("PrefabNames field not found.");
-                }
-            }
-            else
-            {
-                Context.Respond("It's not what I want");
-            }
         }
     }
 }
