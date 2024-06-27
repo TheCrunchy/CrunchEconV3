8e6aeca486e25f51a859718747e87a448914fbc7
TheCrunchy <crunchycamsta@gmail.com>
2024-06-27 21:09:54 +0100

    fdhg

---
+++ CrunchEconContractModels/Patches/DrillCrashFix.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection;
+using CrunchEconV3;
+using Sandbox.Game.Weapons;
+using Sandbox.Game.Weapons.Guns;
+using Sandbox.Game.WorldEnvironment;
+using Sandbox.Game.WorldEnvironment.Modules;
+using Torch.Managers.PatchManager;
+
+using VRage.Utils;
+
+namespace CrunchEconContractModels.Patches
+{
+    public static class DrillCrashFix
+    {
+        private static int patchCount = 0;
+        public static void Patch(PatchContext ctx)
+        {
+            patchCount++;
+            if (patchCount >= 1)
+            {
+                return;
+            }
+            Core.Log.Info("Patching button for grid sales");
+            ctx.GetPattern(methodToPatch).Prefixes.Add(patchMethod);
+        }
+
+
+
+        internal static readonly MethodInfo methodToPatch =
+            typeof(MyDrillBase).GetMethod("DrillEnvironmentSector",
+                BindingFlags.Instance | BindingFlags.NonPublic) ??
+            throw new Exception("Failed to find patch method contract");
+
+        internal static readonly MethodInfo patchMethod =
+            typeof(DrillCrashFix).GetMethod(nameof(DrillEnvironment), BindingFlags.Static | BindingFlags.Public) ??
+            throw new Exception("Failed to find patch method");
+
+        public static bool DrillEnvironment(MyDrillSensorBase.DetectionInfo entry,
+            float speedMultiplier,
+            out MyStringHash targetMaterial)
+        {
+            targetMaterial = MyStringHash.GetOrCompute("Wood");
+            if (patchCount >= 1)
+            {
+                return true;
+            }
+
+            if (entry.Entity == null)
+            {
+                Core.Log.Info("Entry Entity is null");
+                return false;
+            }
+
+            if (entry.Entity as MyEnvironmentSector == null)
+            {
+                Core.Log.Info("Entity as MyEnvironmentSector is null");
+                return false;
+            }
+            MyBreakableEnvironmentProxy module = (entry.Entity as MyEnvironmentSector).GetModule<MyBreakableEnvironmentProxy>();
+            if (module == null)
+            {
+                Core.Log.Info("Module is null");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
