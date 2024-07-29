24e6837d0c1a60e01cb458a4af7476fcd2676245
TheCrunchy <crunchycamsta@gmail.com>
2024-07-29 19:44:46 +0100

    Testing stuff

---
+++ CrunchEconContractModels/DynamicEconomy/KeenStoreSender.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using CoreSystems.Api;
+using Newtonsoft.Json;
+using ProtoBuf;
+
+namespace CrunchEconContractModels.DynamicEconomy
+{
+    public class KeenStoreSender
+    {
+        public Guid ServerId = Guid.NewGuid();
+        public void SendStoreData(List<KeenNPCStoreEntry> entries)
+        {
+            Task.Run(async () =>
+            {
+                var client = new HttpClient();
+                var url = $"https://localhost:7257/api/KeenStores/PostKeenStores";
+                var message = new APIMessage
+                {
+                    ServerId = ServerId, // Assigning a new GUID for demonstration
+                    ServerName = "Crunch Local",
+                    APIKEY = "YourAPIKey"
+                };
+
+                // Serialize the list of KeenNPCStoreEntry to JSON
+                var jsonSerializer = new DataContractJsonSerializer(typeof(List<KeenNPCStoreEntry>));
+                using (var memoryStream = new System.IO.MemoryStream())
+                {
+                    jsonSerializer.WriteObject(memoryStream, entries);
+                    message.JsonMessage = Encoding.UTF8.GetString(memoryStream.ToArray());
+                }
+
+                // Serialize the APIMessage to JSON
+                var messageSerializer = new DataContractJsonSerializer(typeof(APIMessage));
+                string jsonMessage;
+                using (var memoryStream = new System.IO.MemoryStream())
+                {
+                    messageSerializer.WriteObject(memoryStream, message);
+                    jsonMessage = Encoding.UTF8.GetString(memoryStream.ToArray());
+                }
+
+                // Create the HTTP request
+                var content = new StringContent(jsonMessage, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = null;
+                try
+                {
+                    response = await client.PostAsync(url, content); // Synchronously sending the request
+                    response.EnsureSuccessStatusCode();
+
+                    // Output the response for verification
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Request failed: {ex.Message}");
+                }
+                finally
+                {
+                    if (response != null)
+                    {
+                        response.Dispose();
+                    }
+
+                    client.Dispose();
+                }
+            });
+        }
+    }
+
+    public class APIMessage
+    {
+        public Guid ServerId { get; set; }
+        public string ServerName { get; set; }
+        public string APIKEY = "";
+        public string JsonMessage = "";
+    }
+    public class StoreEntry
+    {
+        public string TypeId { get; set; }
+        public string SubTypeId { get; set; }
+        public int Amount { get; set; }
+        public int Price { get; set; }
+        public StoreSaleType SaleType { get; set; }
+        public string GridName { get; set; }
+        public DateTime ExpireAt { get; set; }
+
+    }
+
+    public class KeenNPCStoreEntry : StoreEntry
+    {
+        public long KeenStationId { get; set; }
+        public string OwnerFactionTag { get; set; }
+    }
+
+    public enum StoreSaleType
+    {
+        BuyFromPlayers,
+        SellToPlayers
+    }
+
+}
--- CrunchEconContractModels/DynamicEconomy/StoreLogReader.cs
+++ CrunchEconContractModels/DynamicEconomy/StoreLogReader.cs
@@ -25,98 +25,99 @@ namespace CrunchEconContractModels.DynamicEconomy
     }
 
     [PatchShim]
-        public static class StoreLogReader
+    public static class StoreLogReader
+    {
+
+
+        public static void Patch(PatchContext ctx)
         {
+            Directory.CreateDirectory(NexusPath);
 
-            public static void Patch(PatchContext ctx)
+            Process();
+        }
+
+        public static string NexusPath = $"{Core.path}//EconEntries//";
+        public static void Process()
+        {
+            var files = ProcessFilesInParallel(NexusPath);
+            foreach (var item in files)
             {
-                Directory.CreateDirectory(NexusPath);
-
-                Process();
-            }
-
-            public static void Process()
-            {
-                var files = ProcessFilesInParallel(NexusPath);
-                foreach (var item in files)
-                {
-                    Core.Log.Info($"{item.Key} {item.Value.StoreName} {item.Value.Action} {item.Value.TotalPrice} {item.Value.Amount} ");
-
-                }
+                Core.Log.Info($"{item.Key} {item.Value.StoreName} {item.Value.Action} {item.Value.TotalPrice} {item.Value.Amount} ");
 
             }
 
-            public static ConcurrentDictionary<string, StoreTransaction> ProcessFilesInParallel(string nexusPath)
+        }
+
+        public static ConcurrentDictionary<string, StoreTransaction> ProcessFilesInParallel(string nexusPath)
+        {
+            var transactions = new ConcurrentDictionary<string, StoreTransaction>();
+
+            var files = Directory.GetFiles(nexusPath);
+
+            Parallel.ForEach(files, file =>
             {
-                var transactions = new ConcurrentDictionary<string, StoreTransaction>();
+                var item = File.ReadAllText(file);
+                var parsed = ParseStoreTransaction(item);
+                var key = $"{parsed.StoreName}-{parsed.TypeIdString}-{parsed.SubtypeName}-{parsed.Action}-{parsed.Time:dd-MM-yyyy}";
 
-                var files = Directory.GetFiles(nexusPath);
+                transactions.AddOrUpdate(
+                    key,
+                    parsed,
+                    (existingKey, existingValue) =>
+                    {
+                        existingValue.Amount += parsed.Amount;
+                        existingValue.TotalPrice += parsed.TotalPrice;
+                        return existingValue;
+                    });
+            });
 
-                Parallel.ForEach(files, file =>
-                {
-                    var item = File.ReadAllText(file);
-                    var parsed = ParseStoreTransaction(item);
-                    var key = $"{parsed.StoreName}-{parsed.TypeIdString}-{parsed.SubtypeName}-{parsed.Action}-{parsed.Time:dd-MM-yyyy}";
+            return transactions;
+        }
 
-                    transactions.AddOrUpdate(
-                        key,
-                        parsed,
-                        (existingKey, existingValue) =>
-                        {
-                            existingValue.Amount += parsed.Amount;
-                            existingValue.TotalPrice += parsed.TotalPrice;
-                            return existingValue;
-                        });
-                });
 
-                return transactions;
+        public static StoreTransaction ParseStoreTransaction(string data)
+        {
+            // Split the input string by commas
+            var parts = data.Split(',');
+
+            // Ensure the parts length is as expected (9 parts)
+            if (parts.Length != 11)
+            {
+                throw new ArgumentException("Input string is not in the expected format.");
             }
 
-            public static string NexusPath = $"{Core.path}//EconEntries//";
-
-            public static StoreTransaction ParseStoreTransaction(string data)
+            // Create an instance of StoreTransaction and populate it
+            var transaction = new StoreTransaction
             {
-                // Split the input string by commas
-                var parts = data.Split(',');
+                SteamId = parts[0],
+                Action = parts[1],
+                Amount = int.Parse(parts[2]),
+                TypeIdString = parts[3],
+                SubtypeName = parts[4],
+                TotalPrice = long.Parse(parts[5]),
+                CubeGridEntityId = long.Parse(parts[6]),
+                OwnerFactionTag = parts[7],
+                CubeGridDisplayName = parts[8],
+                StoreName = parts[9],
+                Time = DateTime.Parse(parts[10])
+            };
 
-                // Ensure the parts length is as expected (9 parts)
-                if (parts.Length != 11)
-                {
-                    throw new ArgumentException("Input string is not in the expected format.");
-                }
+            return transaction;
+        }
 
-                // Create an instance of StoreTransaction and populate it
-                var transaction = new StoreTransaction
-                {
-                    SteamId = parts[0],
-                    Action = parts[1],
-                    Amount = int.Parse(parts[2]),
-                    TypeIdString = parts[3],
-                    SubtypeName = parts[4],
-                    TotalPrice = long.Parse(parts[5]),
-                    CubeGridEntityId = long.Parse(parts[6]),
-                    OwnerFactionTag = parts[7],
-                    CubeGridDisplayName = parts[8],
-                    StoreName = parts[9],
-                    Time = DateTime.Parse(parts[10])
-                };
-
-                return transaction;
-            }
-
-            public class StoreTransaction
-            {
-                public string SteamId { get; set; }
-                public string Action { get; set; }
-                public int Amount { get; set; }
-                public string TypeIdString { get; set; }
-                public string SubtypeName { get; set; }
-                public long TotalPrice { get; set; }
-                public long CubeGridEntityId { get; set; }
-                public string OwnerFactionTag { get; set; }
-                public string CubeGridDisplayName { get; set; }
-                public string StoreName { get; set; }
-                public DateTime Time { get; set; }
-            }
+        public class StoreTransaction
+        {
+            public string SteamId { get; set; }
+            public string Action { get; set; }
+            public int Amount { get; set; }
+            public string TypeIdString { get; set; }
+            public string SubtypeName { get; set; }
+            public long TotalPrice { get; set; }
+            public long CubeGridEntityId { get; set; }
+            public string OwnerFactionTag { get; set; }
+            public string CubeGridDisplayName { get; set; }
+            public string StoreName { get; set; }
+            public DateTime Time { get; set; }
         }
     }
+}
