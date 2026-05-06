d9258eb8de50cac36f652914cd2f1b65904055f8
TheCrunchy <crunchycamsta@gmail.com>
2026-05-06 10:01:41 +0100

    comment out obsolete prefab helper

---
+++ CrunchEconContractModels/Random Stuff/ModifiedStoreSearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Sandbox.Game.Entities.Blocks;
+using Sandbox.Game.SessionComponents;
+using Torch.Managers.PatchManager;
+
+namespace CrunchEconContractModels.Random_Stuff
+{
+    [PatchShim]
+    public static class ModifiedStoreSearch
+    {
+        internal static readonly MethodInfo getStores =
+            typeof(MySessionComponentEconomy).GetMethod(
+                "GetPlayersStoreBlocks",
+                BindingFlags.Instance | BindingFlags.NonPublic
+            ) ?? throw new Exception("Failed to find GetPlayersStoreBlocks");
+
+        internal static readonly MethodInfo suffix =
+            typeof(StorePatch).GetMethod(
+                nameof(StorePatch.Suffix),
+                BindingFlags.Static | BindingFlags.Public
+            ) ?? throw new Exception("Failed to find Suffix");
+
+        public static void Patch(PatchContext ctx)
+        {
+            ctx.GetPattern(getStores).Suffixes.Add(suffix);
+        }
+    }
+
+    public static class StorePatch
+    {
+        public static void Suffix(ref HashSet<MyStoreBlock> __result)
+        {
+            
+            var filtered = new HashSet<MyStoreBlock>();
+
+            foreach (var store in __result)
+            {
+                if (store == null)
+                    continue;
+
+                //if its not searchable, skip it 
+                if (!store.DisplayNameText.Contains("!SEARCHABLE!", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    continue;
+                }
+                
+                if (store.IsFunctional)
+                    filtered.Add(store);
+            }
+
+            __result = filtered;
+        }
+    }
+}
--- CrunchEconV3/PlugAndPlay/Prefabs/Helpers/AttackPrefabHelper.cs
+++ CrunchEconV3/PlugAndPlay/Prefabs/Helpers/AttackPrefabHelper.cs
@@ -7,14 +7,14 @@ namespace CrunchEconV3.PlugAndPlay.Prefabs.Helpers
     {
         public override void Init()
         {
-            MyContractEscort myContract = new MyContractEscort();
-            var definition = myContract.GetDefinition();
+            //MyContractEscort myContract = new MyContractEscort();
+            //var definition = myContract.GetDefinition();
 
-            // Get the type of the internal class
-            Type definitionType = definition.GetType();
+            //// Get the type of the internal class
+            //Type definitionType = definition.GetType();
 
-            // Check if the type name matches the internal class name
-            ReflectPrefabs(definitionType, definition, "Sandbox.Definitions.MyContractTypeEscortDefinition", "PrefabsAttackDrones");
+            //// Check if the type name matches the internal class name
+            //ReflectPrefabs(definitionType, definition, "Sandbox.Definitions.MyContractTypeEscortDefinition", "PrefabsAttackDrones");
         }
     }
 }
